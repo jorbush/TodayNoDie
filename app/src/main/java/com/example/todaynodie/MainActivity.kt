0cc3b7aa1687c4b28bd033cd0d73424ed2e20442
jordi <jbonetv5@gmail.com>
2020-02-27 12:05:12 +0100

    8>)

--- app/src/main/java/com/example/todaynodie/MainActivity.kt
+++ app/src/main/java/com/example/todaynodie/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.todaynodie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openSettings(view: View) {
+        val intent = Intent(this,SettingsActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun openSling(view: View) {
+        val intent = Intent(this,SlingActivity::class.java)
+        startActivity(intent)
     }
 
 }
---
+++ app/src/main/java/com/example/todaynodie/TitleActivity.kt
@@ -0,0 +1,19 @@
+package com.example.todaynodie
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class TitleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_title)
+    }
+
+    fun openFirstAid(view: View) {
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
+}
